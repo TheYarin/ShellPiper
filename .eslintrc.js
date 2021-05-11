976c85e2b8a6f45e339858a14b360c1143d0b1aa
Yarin <15931306+TheYarin@users.noreply.github.com>
2021-05-11 15:39:20 +0300

    prettify .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  extends: 'erb',
+  extends: "erb",
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
-    'import/no-extraneous-dependencies': 'off',
-    'class-methods-use-this': 'warn',
-    'no-restricted-syntax': [
+    "import/no-extraneous-dependencies": "off",
+    "class-methods-use-this": "warn",
+    "no-restricted-syntax": [
       // Copied from $ eslint --print-config file.js
-      'error',
+      "error",
       {
-        selector: 'ForInStatement',
+        selector: "ForInStatement",
         message:
-          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+          "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
       },
       // {
       //   selector: 'ForOfStatement',
@@ -18,38 +18,36 @@ module.exports = {
       //     'iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations.',
       // },
       {
-        selector: 'LabeledStatement',
-        message:
-          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+        selector: "LabeledStatement",
+        message: "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
       },
       {
-        selector: 'WithStatement',
-        message:
-          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+        selector: "WithStatement",
+        message: "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
       },
     ],
-    'import/prefer-default-export': 'off',
-    '@typescript-eslint/lines-between-class-members': 'warn',
-    '@typescript-eslint/no-inferrable-types': 'warn',
-    yoda: ['never', { exceptRange: true }],
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/lines-between-class-members": "warn",
+    "@typescript-eslint/no-inferrable-types": "warn",
+    yoda: ["never", { exceptRange: true }],
   },
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module',
-    project: './tsconfig.json',
+    sourceType: "module",
+    project: "./tsconfig.json",
     tsconfigRootDir: __dirname,
     createDefaultProgram: true,
   },
   settings: {
-    'import/resolver': {
+    "import/resolver": {
       // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
       node: {},
       webpack: {
-        config: require.resolve('./.erb/configs/webpack.config.eslint.js'),
+        config: require.resolve("./.erb/configs/webpack.config.eslint.js"),
       },
     },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
     },
   },
 };
